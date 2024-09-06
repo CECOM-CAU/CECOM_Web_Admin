641660cafd547100b3eeedc557a681d53a037dce
yymin1022 <yymin1022@gmail.com>
2024-09-06 15:43:14 +0900

    Update Interface : Added Login Interfaces

--- src/util/Interface.ts
+++ src/util/Interface.ts
@@ -3,3 +3,14 @@ export interface API_RESULT {
     RESULT_MSG: string
     RESULT_DATA: undefined
 }
+
+export interface LoginUser {
+    password: string
+    username: string
+}
+
+export enum LoginResult {
+    LOGIN_OK,
+    LOGIN_FAIL_NO_ACCOUNT,
+    LOGIN_FAIL_PASSWORD,
+}
