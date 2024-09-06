f188e62abbea4fb1645aff9d7170fe492daaa567
yymin1022 <yymin1022@gmail.com>
2024-09-06 16:10:26 +0900

    Update Try Login API : Added Firebase Function Call

--- src/app/api/login/tryLogin/route.ts
+++ src/app/api/login/tryLogin/route.ts
@@ -1,14 +1,27 @@
 import {NextRequest, NextResponse} from "next/server";
-import {API_RESULT} from "@/util/Interface";
+import {API_RESULT, LoginResult, LoginUser} from "@/util/Interface";
 import {corsHeader} from "@/util/CorsUtil";
+import {tryLogin} from "@/util/FirebaseUtil";
 
 export const dynamic = "force-dynamic";
-export async function GET(_: NextRequest) {
+export async function POST(req: NextRequest) {
     const apiResult: API_RESULT = {
         RESULT_CODE: 200,
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
     };
+
+    const loginBody: LoginUser = JSON.parse(await req.text());
+    const loginResult: LoginResult = await tryLogin(loginBody);
+    apiResult.RESULT_DATA = loginResult;
+
+    if(loginResult === LoginResult.LOGIN_OK){
+        apiResult.RESULT_CODE = 200
+    }else if(loginResult === LoginResult.LOGIN_FAIL_NO_ACCOUNT
+        || loginResult === LoginResult.LOGIN_FAIL_PASSWORD){
+        apiResult.RESULT_CODE = 100
+        apiResult.RESULT_MSG = "Failed to Login"
+    }
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
