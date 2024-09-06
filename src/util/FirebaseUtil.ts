c58fb2cd6ef8a76ca87dbde8471d585ef96e4ecd
yymin1022 <yymin1022@gmail.com>
2024-09-06 14:52:01 +0900

    Update FirebaseUtil : Added Firebase Init Functions

---
+++ src/util/FirebaseUtil.ts
@@ -0,0 +1,22 @@
+import {FirebaseApp, initializeApp} from "@firebase/app";
+import {getFirestore} from "@firebase/firestore";
+import { Firestore } from "firebase/firestore";
+import dotenv from "dotenv";
+
+let firebaseApp: FirebaseApp | null = null;
+let firestoreDB: Firestore | null = null;
+
+dotenv.config();
+const firebaseConfig = {
+    apiKey: process.env.FB_API_KEY,
+    authDomain: process.env.FB_AUTH_DOMAIN,
+    projectId: process.env.FB_PROJECT_ID,
+    appId: process.env.FB_APP_ID
+};
+
+const initFirebase = () => {
+    if(firebaseApp == null || firestoreDB == null){
+        firebaseApp = initializeApp(firebaseConfig);
+        firestoreDB = getFirestore(firebaseApp);
+    }
+}
