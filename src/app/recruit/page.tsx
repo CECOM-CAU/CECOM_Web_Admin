75b6a81000fb65511b6be2fb5fb4a8867639c64c
Lim-shweety <smile70200@naver.com>
2024-09-06 14:20:00 +0900

    Update recruit\page.tsx : modify list items

--- src/app/recruit/page.tsx
+++ src/app/recruit/page.tsx
@@ -4,9 +4,25 @@ import ApplicationModal from "@/app/recruit/_component/ApplicationModal";
 import axios from "axios";
 
 const RecruitPage = () => {
-    const header = ['번호', '이름', '학과', '학년', '나이', '학번', '전화번호', '단과대'];
-    const data = [{index:1,name: '김상윤', department: '전자전기',age:'2005',id:'1',college:'소프트웨어대학',grade:'1,2차 학기 ', phone: '01000000000'},
-        {index:2,name: '유용민', department: '소프트웨어',age:'2005',id:'1',college:'소프트웨어대학',grade:'1,2차 학기 ', phone: '01000000000'}]
+    const header = ['번호', '이름', '학과', '학년',  '학번'];
+    const data = [{
+        name: '김상윤',
+        department: '전자전기',
+        age: '2005',
+        id: '20201234',
+        college: '소프트웨어대학',
+        grade: '1,2차 학기 ',
+        phone: '01000000000'
+    },
+        {
+            name: '유용민',
+            department: '소프트웨어',
+            age: '2005',
+            id: '20201234',
+            college: '소프트웨어대학',
+            grade: '1,2차 학기 ',
+            phone: '01000000000'
+        }]
     const [isOpen, setIsOpen] = useState<boolean>(false);
     const [currentIndex, setCurrentIndex] = useState<number>(1);
     const modalRef = useRef<HTMLDivElement>(null);
@@ -33,38 +49,36 @@ const RecruitPage = () => {
     }
     return (
         <>
-            <div className='flex flex-col w-full items-center mt-[70px]'>
-                <div className='text-[30px] my-[20px]'>Recruit</div>
-                <table>
-                    <thead>
-                    <tr>
-                        {
-                            header.map((h, index) => (
-                                <th className=' text-start pl-[5px] pr-[20px] border-[1px]' key={index}>{h}</th>
-                            ))
-                        }
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {data.map((d, index) => (
-                        <tr className='' key={index} onClick={() => modalOpen(index)}>
-
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{index}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['name']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['department']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['grade']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['age']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['id']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['phone']}</td>
-                            <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{d['college']}</td>
-
-
+            <div className='flex flex-col w-full items-center mt-[20px] md:mt-[70px] mx-[5px]'>
+                <div className='text-[30px] my-[10px] md:my-[20px]'>Recruit</div>
+                <div className='overflow-x-auto'>
+                    <table className=''>
+                        <thead>
+                        <tr>
+                            {
+                                header.map((h, index) => (
+                                    <th className='text-start pl-[5px] pr-[20px] border-[1px] bg-[#d9d9d950] break-keep'
+                                        key={index}>{h}</th>
+                                ))
+                            }
                         </tr>
-                    ))}
-                    </tbody>
+                        </thead>
+                        <tbody>
+                        {data.map((d, index) => (
+                            <tr className='' key={index} onClick={() => modalOpen(index)}>
 
-                </table>
+                                <td className='text-start pl-[5px] pr-[20px] border-[1px]'>{index + 1}</td>
+                                <td className='text-start pl-[5px] pr-[20px] border-[1px] break-keep'>{d['name']}</td>
+                                <td className='text-start pl-[5px] pr-[20px] border-[1px] break-keep'>{d['department']}</td>
+                                <td className='text-start pl-[5px] pr-[20px] border-[1px] break-keep'>{d['grade']}</td>
+                                <td className='text-start pl-[5px] pr-[20px] border-[1px] break-keep'>{d['id']}</td>
 
+
+                            </tr>
+                        ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
             {isOpen ? (
                 <ApplicationModal setOpen={setIsOpen} modalRef={modalRef} modalHandler={modalOutsideClick}
