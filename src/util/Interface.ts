029793aa39258550c9a3a73e748ef194e42f8ede
yymin1022 <yymin1022@gmail.com>
2024-09-10 19:42:21 +0900

    Update Interface : Migrated Recruit Interfaces

--- src/util/Interface.ts
+++ src/util/Interface.ts
@@ -1,7 +1,7 @@
 export interface API_RESULT {
     RESULT_CODE: number
     RESULT_MSG: string
-    RESULT_DATA: LoginResult | undefined
+    RESULT_DATA: LoginResult | RecruitSubmissionDetail | RecruitSubmissionList | undefined
 }
 
 export interface LoginUser {
@@ -13,4 +13,35 @@ export const enum LoginResult {
     LOGIN_OK = "OK",
     LOGIN_FAIL_NO_ACCOUNT = "FAIL_NO_ACCOUNT",
     LOGIN_FAIL_PASSWORD = "FAIL_PASSWORD",
+}
+
+export interface RecruitSubmissionDetail {
+    age: string
+    answer: Array<string>
+    college: string
+    department: string
+    grade: string
+    id: string
+    isPrivacyCollectAgree: boolean
+    name: string
+    phone: string
+    timestamp: number
+}
+
+export interface RecruitSubmissionItem {
+    age: string
+    answer: Array<string>
+    college: string
+    department: string
+    grade: string
+    id: string
+    isPrivacyCollectAgree: boolean
+    name: string
+    phone: string
+    timestamp: number
+}
+
+export interface RecruitSubmissionList {
+    count: number
+    data: Array<RecruitSubmissionItem>
 }
