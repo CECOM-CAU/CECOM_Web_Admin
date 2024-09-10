0934590ff0fed09e2102028d6bbe99cf5810e226
Lim-shweety <smile70200@naver.com>
2024-09-10 22:23:32 +0900

    Update main\page.tsx : add check login status and modify text

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+import {useEffect} from "react";
+
 const MainPage = () => {
+    useEffect(() => {
+        if (sessionStorage.getItem('username') === null || sessionStorage.getItem('password') === null) {
+            window.location.replace('/login');
+        }
+    }, []);
     return (
         <div>
-            <h1>Recruit</h1>
+            <h1>MainPage Editor</h1>
         </div>
     );
 }
