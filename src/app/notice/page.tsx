197e5fcb9638f6c6e0a79376cc460442b0068aec
Lim-shweety <smile70200@naver.com>
2024-09-10 22:24:09 +0900

    Update notice\page.tsx : add check login status and modify text

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -1,7 +1,15 @@
+'use client';
+import {useEffect} from "react";
+
 const NoticePage = () => {
+    useEffect(() => {
+        if (sessionStorage.getItem('username') === null || sessionStorage.getItem('password') === null) {
+            window.location.replace('/login');
+        }
+    }, []);
     return (
         <div>
-            <h1>Recruit</h1>
+            <h1>Notice</h1>
         </div>
     );
 }
