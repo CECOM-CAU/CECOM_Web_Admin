777786c6c0b76e29e9d97f179e7d5556fae11293
yymin1022 <yymin1022@gmail.com>
2024-09-10 23:42:27 +0900

    Update Recruit Availability API : Added Firebase Function Call

--- src/app/api/recruit/getAvailability/route.ts
+++ src/app/api/recruit/getAvailability/route.ts
@@ -1,7 +1,12 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT, LoginResult, LoginUser} from "@/util/Interface";
 import {corsHeader} from "@/util/CorsUtil";
-import {checkLogin, getRecruitQuestionList, getRecruitSubmissionList} from "@/util/FirebaseUtil";
+import {
+    checkLogin,
+    getRecruitAvailability,
+    getRecruitQuestionList,
+    getRecruitSubmissionList
+} from "@/util/FirebaseUtil";
 
 export const dynamic = "force-dynamic";
 export async function GET(_: NextRequest) {
@@ -10,6 +15,8 @@ export async function GET(_: NextRequest) {
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
+
+    apiResult.RESULT_DATA = await getRecruitAvailability();
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
