7693345961ca4140d97f2f412ab15788ebff8760
yymin1022 <yymin1022@gmail.com>
2024-09-10 19:43:31 +0900

    Update Submission List API : Migrated Logics from Legacy

--- src/app/api/recruit/getSubmissionList/route.ts
+++ src/app/api/recruit/getSubmissionList/route.ts
@@ -1,6 +1,25 @@
 import {NextRequest, NextResponse} from "next/server";
+import {API_RESULT} from "@/util/Interface";
+import {corsHeader} from "@/util/CorsUtil";
+import {getRecruitSubmissionList} from "@/util/FirebaseUtil";
 
 export const dynamic = "force-dynamic";
 export async function GET(_: NextRequest) {
-    return NextResponse.json({ status: 200 });
+    const apiResult: API_RESULT = {
+        RESULT_CODE: 200,
+        RESULT_MSG: "Success",
+        RESULT_DATA: undefined
+    }
+
+    const submissionList = await getRecruitSubmissionList();
+    if(submissionList === undefined || submissionList.data.length == 0){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
+    }
+
+    apiResult.RESULT_DATA = submissionList;
+
+    return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
