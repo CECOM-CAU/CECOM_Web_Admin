1ff9b07cd791ac8982f65b1cb8667babce074030
yymin1022 <yymin1022@gmail.com>
2024-09-10 23:40:05 +0900

    Update Recruit Question API : Added Firebase Function Call

--- src/app/api/recruit/getQuestionList/route.ts
+++ src/app/api/recruit/getQuestionList/route.ts
@@ -1,7 +1,7 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_RESULT, LoginResult, LoginUser} from "@/util/Interface";
 import {corsHeader} from "@/util/CorsUtil";
-import {checkLogin, getRecruitSubmissionList} from "@/util/FirebaseUtil";
+import {checkLogin, getRecruitQuestionList, getRecruitSubmissionList} from "@/util/FirebaseUtil";
 
 export const dynamic = "force-dynamic";
 export async function GET(_: NextRequest) {
@@ -10,6 +10,16 @@ export async function GET(_: NextRequest) {
         RESULT_MSG: "Success",
         RESULT_DATA: undefined
     }
+
+    const questionList = await getRecruitQuestionList();
+    if(questionList === undefined || questionList.count == 0){
+        apiResult.RESULT_CODE = 100;
+        apiResult.RESULT_MSG = "An Error Occurred";
+        apiResult.RESULT_DATA = undefined;
+        return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
+    }
+
+    apiResult.RESULT_DATA = questionList;
 
     return NextResponse.json(apiResult, { status: 200, headers: corsHeader });
 }
